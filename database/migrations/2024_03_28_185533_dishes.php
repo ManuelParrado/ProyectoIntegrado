b9c3d10a4b7d82fce5a5c77b8e0acb5bbfd0e9ba
ManuelParrado <manuparradobarrero2004@gmail.com>
2024-05-03 14:27:00 +0200

    Cambios en el nav

--- database/migrations/2024_03_28_185533_dishes.php
+++ database/migrations/2024_03_28_185533_dishes.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,12 +16,100 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('type');
-            $table->integer('price');
-            $table->string('image');
+            $table->decimal('price', 8, 2);
+            $table->string('image')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
+
+        DB::table('dishes')->insert([
+            [
+                'name' => 'Nachos con Queso',
+                'type' => 'appetizer',
+                'price' => 10.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Aros de Cebolla Crujientes',
+                'type' => 'appetizer',
+                'price' => 8.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Pollo a la Parrilla con Verduras',
+                'type' => 'main_course',
+                'price' => 16.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Pasta Carbonara',
+                'type' => 'main_course',
+                'price' => 14.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Sándwich Club',
+                'type' => 'main_course',
+                'price' => 12.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Salmón a la Parrilla',
+                'type' => 'main_course',
+                'price' => 15.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Helado Sundae',
+                'type' => 'dessert',
+                'price' => 6.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Tarta de Limón',
+                'type' => 'dessert',
+                'price' => 7.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Ceviche de Camarón',
+                'type' => 'appetizer',
+                'price' => 12.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Lomo Saltado',
+                'type' => 'main_course',
+                'price' => 18.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Arroz con Mariscos',
+                'type' => 'main_course',
+                'price' => 20.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Tacos de Carnitas',
+                'type' => 'main_course',
+                'price' => 14.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Pastel de Chocolate',
+                'type' => 'dessert',
+                'price' => 8.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+            [
+                'name' => 'Flan de Vainilla',
+                'type' => 'dessert',
+                'price' => 7.99,
+                'image' => 'ruta/de/la/imagen.jpg'
+            ],
+        ]);
     }
+
 
     /**
      * Reverse the migrations.
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -19,22 +19,59 @@ new class extends Component
 <nav x-data="{ open: false }" class="bg-gradient-to-b from-white to-gray-300 shadow-lg p-1">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-center h-24">
-            <div>
-                <!-- Logo -->
-                <div class="shrink-0">
-                    @auth
-                        <a href="{{ route('dashboard') }}" wire:navigate>
-                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                        </a>
-                    @else
-                        <a href="{{ route('welcome') }}" wire:navigate>
-                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                        </a>
-                    @endauth
+        <div class="flex justify-between h-24">
+            <div class="flex space-x-6">
+                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                    {{ __('Entrantes') }}
+                </x-nav-link>
+                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                    {{ __('Principales') }}
+                </x-nav-link>
+                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                    {{ __('Postres') }}
+                </x-nav-link>
+            </div>
+            <!-- Logo -->
+            <div class="flex items-center">
+                @auth
+                    <a href="{{ route('dashboard') }}" wire:navigate>
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    </a>
+                @else
+                    <a href="{{ route('welcome') }}" wire:navigate>
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    </a>
+                @endauth
+            </div>
+            <div class="flex space-x-6">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                            {{ __('Reservas') }}
+                        </x-nav-link>
+                    </x-slot>
 
-                </div>
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile')" wire:navigate>
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <button wire:click="logout" class="w-full text-start">
+                            <x-dropdown-link>
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </button>
+                    </x-slot>
+                </x-dropdown>
+
+                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                    {{ __('Inicia Sesión') }}
+                </x-nav-link>
+                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                    {{ __('Registrate') }}
+                </x-nav-link>
             </div>
         </div>
     </div>
 </nav>
+
