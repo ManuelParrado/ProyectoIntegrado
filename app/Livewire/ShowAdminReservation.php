6d084fb04afe865398ad05c68ef5350b1254b01c
ManuelParrado <manuparradobarrero2004@gmail.com>
2024-05-30 19:27:41 +0200

    Arreglo de la administracion de reservas y comienzo de filtros

--- app/Livewire/ShowAdminReservation.php
+++ app/Livewire/ShowAdminReservation.php
@@ -2,48 +2,56 @@
 
 namespace App\Livewire;
 
-use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Carbon\Carbon;
 
 class ShowAdminReservation extends Component
 {
-
     use WithPagination;
+
 
     public $isReservationAdministration = false;
     public $selected_date;
+    public $filter;
+
+
     public $totalCapacityByTimeslot = 0;
+    public $totalCapacity = 0;
+    public $groupedReservations;
+
 
     public function mount()
     {
-        //$this->selected_date = now()->format('m-d-Y');
-        $this->selected_date = '05-27-2024';
+        $this->searchReservations();
+        $this->filter = 'all';
+        $this->totalCapacity = 0;
+        $this->selected_date = now()->format('Y-m-d');
         $this->isReservationAdministration = true;
-    }
-
-    #[On('dateSelected')]
-    public function updateDate($date)
-    {
-        $this->selected_date = $date;
     }
 
     public function render()
     {
-        $groupedReservations = $this->searchReservations();
-        return view(
-            'livewire.web_restaurant.admin.show-admin-reservation',
-            ['groupedReservations' => $groupedReservations]
-        );
+        return view('livewire.web_restaurant.admin.show-admin-reservation', [
+            'groupedReservations' => $this->groupedReservations,
+        ]);
+    }
+
+    public function updatedFilter()
+    {
+        $this->searchReservations(); // Actualiza reservas cuando el filtro cambia.
+    }
+
+    public function updatedSelectedDate()
+    {
+        $this->searchReservations(); // Actualiza reservas cuando la fecha seleccionada cambia.
     }
 
     public function searchReservations()
     {
-        $selected_date = \Carbon\Carbon::createFromFormat('m-d-Y', $this->selected_date)->format('Y-m-d');
-
-        return DB::table('table_user')
+        $query = DB::table('table_user')
             ->join('users', 'table_user.user_id', '=', 'users.id')
             ->join('tables', 'table_user.table_id', '=', 'tables.id')
             ->select(
@@ -54,11 +62,35 @@ class ShowAdminReservation extends Component
                 'users.telephone_number as user_telephone',
                 'tables.number as table_number',
                 'tables.capacity as table_capacity',
+                'table_user.timeslot'
             )
-            ->where('table_user.date', $selected_date)
-            ->orderBy('table_user.timeslot')
-            ->get()
-            ->groupBy('timeslot');
+            ->where('table_user.date', $this->selected_date);
+
+        // Aplica el filtro de timeslot según la selección del usuario
+        if ($this->filter === 'day') {
+            $query->whereIn('table_user.timeslot', [
+                '12:00 - 13:00', '13:00 - 14:00', '14:00 - 15:00', '15:00 - 16:00'
+            ]);
+        } elseif ($this->filter === 'night') {
+            $query->whereIn('table_user.timeslot', [
+                '20:00 - 21:00', '21:00 - 22:00', '22:00 - 23:00', '23:00 - 00:00'
+            ]);
+        }
+
+        $reservations = $query->orderBy('table_user.timeslot')->get();
+
+        // Agrupar los resultados por 'timeslot'
+        $this->groupedReservations = $reservations->groupBy('timeslot');
+
+        // Calcular la capacidad total
+        $this->totalCapacity = $reservations->sum('table_capacity');
+    }
+
+
+
+    public function setFilter($filter)
+    {
+        $this->filter = $filter;
     }
 
     #[On('showReservationAdministration')]
--- resources/views/livewire/web_restaurant/admin/show-admin-reservation.blade.php
+++ resources/views/livewire/web_restaurant/admin/show-admin-reservation.blade.php
@@ -2,21 +2,31 @@
     <div class="py-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-lg">
             <h1 class="text-3xl font-bold text-gray-800 p-2">Administracion de reservas</h1>
-            <div class="flex overflow-hidden shadow-md rounded-md bg-gray-200">
+            <div class="flex overflow-hidden shadow-md rounded-md bg-gray-200 ">
                 <aside id="separator-sidebar" class="bg-gray-50 p-4 w-1/3" aria-label="Sidebar">
                     <div class="h-full overflow-y-auto">
-                        <ul class="space-y-2 font-medium">
+                        <ul class="space-y-3 font-medium">
                             <li>
-                                {{ $selected_date }}
+                                <label class='block' for="calendar1">Seleccione un día: </label>
+                                <input type="date" wire:model.live='selected_date' id="calendar1" class="bg-gray-100 rounded-md focus:ring-gray-300">
                             </li>
                             <li>
-                                <div class="relative max-w-sm">
-                                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                    <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                                        </svg>
-                                    </div>
-                                    <input id='datepicker' datepicker  type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="Selecciona el día">
+                                <div class="container">
+                                    <form>
+                                        <label>
+                                            <input type="radio" wire:click='setFilter("all")' name="radio" checked>
+                                            <span>Todos los horarios</span>
+                                        </label>
+                                        <label>
+                                            <input type="radio" wire:click='setFilter("day")'' name="radio">
+                                            <span>Horario de día</span>
+                                        </label>
+                                        <label>
+                                            <input type="radio" wire:click='setFilter("night")' value="night" name="radio">
+                                            <span>Horario de noche</span>
+                                        </label>
+                                        {{$filter}}
+                                    </form>
                                 </div>
                             </li>
                         </ul>
@@ -24,18 +34,31 @@
                 </aside>
                 <div class="w-full h-auto flex justify-center items-center">
                     <div class="w-full p-4 h-full space-y-4 flex-row text-lg">
+                        <div wire:loading class="bg-gray-50 p-4 w-full shadow-md rounded-md flex justify-around items-center">
+                            Cargando ...
+                        </div>
                         @if($groupedReservations->isEmpty())
-                            <p class="bg-gray-50 p-4 w-full shadow-md rounded-md flex justify-around items-center">
-                                Ninguna reserva para el día {{$selected_date}}
-                            </p>
+                            <div wire:loading.remove class="bg-gray-50 p-4 w-full shadow-md rounded-md flex justify-around items-center">
+                                Ninguna reserva para el día {{\Illuminate\Support\Carbon::parse($selected_date)->format('d/m/Y')}}
+                            </div>
                         @else
+                            <div wire:loading.remove class="bg-gray-50 p-4 w-full shadow-md rounded-md flex justify-around items-center">
+                                Total de personas: {{$totalCapacity}}
+                            </div>
                             @foreach($groupedReservations as $timeslot => $reservations)
-                                @php $totalCapacityByTimeslot = 0; @endphp <!-- Inicializa la variable antes del bucle -->
-                                <div class="space-y-2">
-                                    <button type="button" class="bg-gray-50 py-4 px-6 w-full shadow-md rounded-md flex justify-between items-center cursor-pointer"
-                                        aria-controls="dropdown-{{ $selected_date }}-{{ str_replace(' ', '', $timeslot) }}"
-                                        data-collapse-toggle="dropdown-{{ $selected_date }}-{{ str_replace(' ', '', $timeslot) }}">
-                                        <h2>{{ $timeslot }} - Total de Personas: {{$totalCapacityByTimeslot}}</h2>
+                                @php $totalCapacityByTimeslot = 0; @endphp
+                                @foreach($reservations as $reservation)
+                                    @php
+                                        $totalCapacityByTimeslot += $reservation->table_capacity;
+                                        $totalCapacity += $totalCapacityByTimeslot;
+                                    @endphp
+                                @endforeach
+                                <div wire:loading.remove class="space-y-2">
+                                    <button onclick="collapse('dropdown-{{ $selected_date }}-{{ str_replace(' ', '', $timeslot) }}')"
+                                        id='button-{{ $selected_date }}-{{ str_replace(' ', '', $timeslot) }}'
+                                        type="button"
+                                        class="bg-gray-50 py-4 px-6 w-full shadow-md rounded-md flex justify-between items-center cursor-pointer">
+                                        <h2>{{ $timeslot }} - Personas: {{$totalCapacityByTimeslot}}</h2>
                                         <div>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
@@ -67,27 +90,19 @@
                                 </div>
                             @endforeach
                         @endif
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @script
     <script>
-        const datepickerEl = document.getElementById('datepicker');
-
-        datepickerEl.addEventListener('changeDate', (event) => {
-            const date = new Date(event.detail.date);
-
-            const day = String(date.getDate()).padStart(2, '0');
-            const month = String(date.getMonth() + 1).padStart(2, '0');
-            const year = date.getFullYear();
-
-            $wire.dispatch('dateSelected', { date: `${month}-${day}-${year}` }, false);
-
-            cleanup();
-        });
+        function collapse(id) {
+            var dropdown = document.getElementById(id);
+            if (dropdown.classList.contains('hidden')) {
+                dropdown.classList.remove('hidden');
+            } else {
+                dropdown.classList.add('hidden');
+            }
+        }
     </script>
-    @endscript
 </div>
